0f5b83f9ffc3cbab216f7673c20de7470ab5c174
unknown <puneetrihan.che18@itbhu.ac.in>
2021-05-11 04:07:41 +0530

    ughhh

--- app.js
+++ app.js
@@ -184,6 +184,7 @@ app.post("/login", function (req, res) {
   });
 });
 
-app.listen(process.env.PORT || 3000, function () {
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
   console.log("Server started on port 3000.");
 });
