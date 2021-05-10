3606a133929832f5930aec175897a19729ce0bc3
unknown <puneetrihan.che18@itbhu.ac.in>
2021-05-11 04:27:38 +0530

    idk

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ passport.use(
     {
       clientID: process.env.CLIENT_ID,
       clientSecret: process.env.CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/auth/google/secrets",
+      callbackURL: "http://puneetgotsecrets.herokuapp.com/auth/google/secrets",
       userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
     },
     function (accessToken, refreshToken, profile, cb) {
